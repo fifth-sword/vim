c26fff068444c676af3e87d192c4249be46bbbb5
fifth-sword <fifth.sword@gmail.com>
2012-07-01 11:32:10 +0900

    localが関数内でしか使用できないため、関数呼び出しに変更。

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
-local current=$(cd $(dirname $0) && pwd)
-ln -sF $current/.vimrc $HOME
-ln -sF $current/.vim $HOME
+install() {
+  local current=$(cd $(dirname $0) && pwd)
+  ln -sF $current/.vimrc $HOME
+  ln -sF $current/.vim $HOME
+}
+install
